b90ad379d524b780e18e534abbfb2b0e719374d0
LeilaAfhim <leilaafhim@gmail.com>
2021-01-19 10:05:14 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Weather from "./Weather";
 
 function App() {
   return (
+    <div className="App">
         <div className="container" >
         <div className="weather-app-wrapper">
 <div className="weather-app">
@@ -13,6 +14,7 @@ function App() {
 </div> 
     </div>
    <a href="https://github.com/LeilaAfhim/react-weather-app" className="open-source">Open-source code </a>by <a href="https://www.leilaafhim.com" className="website">Leila Afhim</a> 
+    </div>
     </div>
 
   );
